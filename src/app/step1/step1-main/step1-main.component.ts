d357a19bf25d768e11b1e6b4626577a11de3354c
jiho <ghwlchlaks@naver.com>
2018-12-14 00:17:48 +0900

    data sharing @ViewChild @ViewChildren 직접 component객체 참조, DOM객체 참조하여 제어

--- src/app/step1/step1-main/step1-main.component.ts
+++ src/app/step1/step1-main/step1-main.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
+//child view에 객체를 직접 접근 하기위한 참조
+import {Step1SearchComponent} from '../step1-search/step1-search.component';
 
 @Component({
   selector: 'app-step1-main',
@@ -31,5 +33,37 @@ export class Step1MainComponent implements OnInit {
 
   changeTitleBar(searchInfo) : void {
     this.searchTitle = `${searchInfo.keyword} ( ${searchInfo.category})`;
+  }
+
+  /*
+  자식 component 객체에 `직접` 접근하는 방법
+  @viewchild decorator를 이용할 경우 부합되는 객체 한개만 접근가능
+  @viewChildren decorater를 이용할 경우 부합되는 객체를 모두 참음. QueryList형태로 객체집합을 얻을수 있음. toArray()메소드를 통해 배열을 얻어내서 사용
+  하지만 Component의 view가 변경되면 Component를 처리하는 부분도 맞게 바뀌어야한다. component의 재사용성과 유지보수성에 문제가 생길 여지가 있으므로 지양하는 것이 좋음
+  */
+  @ViewChild(Step1SearchComponent) searchComp : Step1SearchComponent;
+  @ViewChildren(Step1SearchComponent) searchCompArr : QueryList<Step1SearchComponent>;
+  clearCondition(): void {
+    this.selectedValue = null;
+    this.searchTitle = null;
+    /*
+      @viewChild를 사용할 경우
+      this.searchComp._bookCategory = null;
+      this.searchComp.keyword = null;
+    */
+    // @ViewChildren을 사용할 경우 
+    this.searchCompArr.toArray()[0]._bookCategory = null;
+    this.searchCompArr.toArray()[0].keyword = null;
+  }
+
+  /*
+    #resultStatus값을 참조하여 DOM을 직접 제어할 수 있다.
+  */
+  @ViewChild('resultStatus') resultToolbar : ElementRef;
+  changeDOM(): void {
+    this.resultToolbar.nativeElement.onclick = function() {
+      alert("DOM을 직접 제어할 수 있어요");
+    }
+    this.resultToolbar.nativeElement.innerHTML = "클릭해보세요";
   }
 }
